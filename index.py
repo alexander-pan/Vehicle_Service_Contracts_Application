3190f6c861ff467bd1b39d9ab54b002c1eacdd14
alexander-pan <alex.pan@statusquota.co>
2018-07-30 20:36:46 +0000

    added actual link names to app pages

--- index.py
+++ index.py
@@ -10,19 +10,19 @@ from apps import app3,app4,app5,app6,app7,app9,app10
 
 home = html.Div([
     html.H3('Home Page'),
-    dcc.Link('Admin Bank Reconcillation Summary', href='/apps/app3'),
+    dcc.Link('Admin Bank Reconcillation Summary', href='/abr_summary'),
     html.Br(),
-    dcc.Link('Admin Bank Reconcillation Details', href='/apps/app4'),
+    dcc.Link('Admin Bank Reconcillation Details', href='/abr_details'),
     html.Br(),
-    dcc.Link('Funder Bank Reconcillation Summary', href='/apps/app5'),
+    dcc.Link('Funder Bank Reconcillation Summary', href='/fbr_summary'),
     html.Br(),
-    dcc.Link('Funder Bank Reconcillation Details', href='/apps/app6'),
+    dcc.Link('Funder Bank Reconcillation Details', href='/fbr_details'),
     html.Br(),
-    dcc.Link('Scenario Modeling Summary', href='/apps/app7'),
+    dcc.Link('Scenario Modeling Summary', href='/scenario_summary'),
     html.Br(),
-    dcc.Link('Admin Scenario Modeling Details', href='/apps/app9'),
+    dcc.Link('Admin Scenario Modeling Details', href='/adminscenario_details'),
     html.Br(),
-    dcc.Link('Vendor Scenario Modeling Details', href='/apps/app10')
+    dcc.Link('Vendor Scenario Modeling Details', href='/vendorscenario_details')
 ])
 
 app.layout = html.Div([
@@ -35,19 +35,19 @@ app.layout = html.Div([
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/apps/app3':
+    if pathname == '/abr_summary':
         return app3.layout_page
-    elif pathname == '/apps/app4':
+    elif pathname == '/abr_details':
         return app4.layout_page
-    elif pathname == '/apps/app5':
+    elif pathname == '/fbr_summary':
         return app5.layout_page
-    elif pathname == '/apps/app6':
+    elif pathname == '/fbr_details':
         return app6.layout_page
-    elif pathname == '/apps/app7':
+    elif pathname == '/scenario_summary':
         return app7.layout_page
-    elif pathname == '/apps/app9':
+    elif pathname == '/adminscenario_details':
         return app9.layout_page
-    elif pathname == '/apps/app10':
+    elif pathname == '/vendorscenario_details':
         return app10.layout_page
     else:
         return home
